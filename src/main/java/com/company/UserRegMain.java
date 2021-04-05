22d858e972a0cc447d5b5a9b9f2e06505d98b4d0
MalavikaAjit <malavikaajit9@gmail.com>
2021-04-05 14:12:55 +0530

    ADD : added feature to validate Password rule 4

--- src/main/java/com/company/UserRegMain.java
+++ src/main/java/com/company/UserRegMain.java
@@ -27,7 +27,7 @@ public class UserRegMain {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter email");
         String email =scan.next();
-        String pattern = "^[a-zA-Z0-9]+([.+_-]{0,1}+[a-zA-Z0-9])*+[@]+[a-zA-Z]+[.]+[a-zA-Z]{2,4}+([.]{1}+[a-zA-Z]{0,2})*";
+        String pattern =  "^[a-zA-Z0-9]+([.+_-]{0,1}+[a-zA-Z0-9])*+[@]+[a-zA-Z]+[.]+[a-zA-Z]{2,4}+([.]{1}+[a-zA-Z]{0,2})*";
         System.out.println("email is " +email);
         System.out.println("Is the email valid? " + email.matches(pattern));
 
@@ -45,7 +45,7 @@ public class UserRegMain {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter password");
         String password =scan.next();
-        String pattern =  "^(.*[A-Z]{1,}[0-9]{1,}[A-Za-z0-9]{6,})$";
+        String pattern =  "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$";
         System.out.println("password is " +password);
         System.out.println("Is the password valid? " + password.matches(pattern));
 
