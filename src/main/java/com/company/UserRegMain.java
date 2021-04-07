530670f70b5322bba7c395011b9d23e8e21ce363
MalavikaAjit <malavikaajit9@gmail.com>
2021-04-07 15:34:44 +0530

    ADD: added again

--- src/main/java/com/company/UserRegMain.java
+++ src/main/java/com/company/UserRegMain.java
@@ -2,7 +2,7 @@ package com.company;
 
 import java.util.Scanner;
 import java.util.regex.Pattern;
-
+git
 public class UserRegMain {
     //validating user input first name
     public static void validateFirstName() {
